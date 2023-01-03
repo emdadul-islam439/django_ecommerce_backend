1ff203bcc178307ac0479457707d1e483f473f69
Emdadul_Islam <emdadulislam439@gmail.com>
2023-01-04 02:21:23 +0600

    added code for automatically importing signals.py files while starting related apps

--- ecommerce_backend/background_task_app/apps.py
+++ ecommerce_backend/background_task_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class BackgroundTaskAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'background_task_app'
+    
+    def ready(self):
+        import background_task_app.signals
