314ccb943dcbf489880a970bbb59cc8cef7f2cf4
Emdadul_Islam <emdadulislam439@gmail.com>
2023-01-10 21:44:36 +0600

    fixed 'import error' at settings.py

--- ecommerce_backend/ecommerce_backend/settings.py
+++ ecommerce_backend/ecommerce_backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 import os
-import settings_secret
+from . import settings_secret
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
