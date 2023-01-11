57bc86d30b0e72f54c031b3fcb13f0728d98a5c3
Emdadul_Islam <emdadulislam439@gmail.com>
2023-01-12 04:46:40 +0600

    some minor changes

--- ecommerce_backend/customer_app/api/urls.py
+++ ecommerce_backend/customer_app/api/urls.py
@@ -1,16 +1,15 @@
-from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
+from django.urls import path
 from django.contrib.auth import views as auth_views
 
 from customer_app.api import views as customer_views
 
 urlpatterns = [
     path('register/', customer_views.register, name = "register"),
+    path('login/', auth_views.LoginView.as_view(template_name = "customer_app/login.html"), name = "login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name = "customer_app/logout.html"), name = "logout"),
+    
     path('profile/', customer_views.profile, name = "profile"),
     path('wishlist/', customer_views.wishList, name = 'wishlist'),
     path('order-list/', customer_views.orderList, name = 'order-list'),
     path('order-details/<int:pk>', customer_views.OrderDetailView.as_view(), name = 'order-details'),
-    path('login/', auth_views.LoginView.as_view(template_name = "customer_app/login.html"), name = "login"),
-    path('logout/', auth_views.LogoutView.as_view(template_name = "customer_app/logout.html"), name = "logout"),
 ]
