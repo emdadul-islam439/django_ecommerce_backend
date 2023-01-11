4528d62ba457a366976f897bc46005eea2db216b
Emdadul_Islam <emdadulislam439@gmail.com>
2023-01-12 04:36:10 +0600

    solved login and logout templates not found issue

--- ecommerce_backend/customer_app/api/urls.py
+++ ecommerce_backend/customer_app/api/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('wishlist/', customer_views.wishList, name = 'wishlist'),
     path('order-list/', customer_views.orderList, name = 'order-list'),
     path('order-details/<int:pk>', customer_views.OrderDetailView.as_view(), name = 'order-details'),
-    path('login/', auth_views.LoginView.as_view(template_name = "customers/login.html"), name = "login"),
-    path('logout/', auth_views.LogoutView.as_view(template_name = "customers/logout.html"), name = "logout"),
+    path('login/', auth_views.LoginView.as_view(template_name = "customer_app/login.html"), name = "login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name = "customer_app/logout.html"), name = "logout"),
 ]
