1f751330fd3c5feded2038024639a3cd9ae8c11f
Emdadul_Islam <emdadulislam439@gmail.com>
2023-01-13 04:08:25 +0600

    re-ordered serializers for ease of use

--- ecommerce_backend/store_app/api/serializers.py
+++ ecommerce_backend/store_app/api/serializers.py
@@ -8,15 +8,16 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class CartSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cart
-        fields = '__all__'
-
-
 class CartItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = CartItem
+        fields = '__all__'
+
+
+class CartSerializer(serializers.ModelSerializer):
+    cart_item_list = CartItemSerializer(many=True, read_only=True)
+    class Meta:
+        model = Cart
         fields = '__all__'
 
 
@@ -26,15 +27,16 @@ class ShippingAddressSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class OrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order
-        fields = '__all__'
-
-
 class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
+        fields = '__all__'
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    order_item_list = OrderItemSerializer(many=True, read_only=True)
+    class Meta:
+        model = Order
         fields = '__all__'
 
 
