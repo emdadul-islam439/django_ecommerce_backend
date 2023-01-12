06c8e5425bf1dc300355e569fb8d05ff05bbb0c4
Emdadul_Islam <emdadulislam439@gmail.com>
2023-01-13 03:43:55 +0600

    added skeleton code for different serializers of store_app

---
+++ ecommerce_backend/store_app/api/serializers.py
@@ -0,0 +1,62 @@
+from rest_framework import serializers
+
+from store_app.models import Product, Cart, CartItem, ShippingAddress, Order, OrderItem, WishListItem, PurchasedItem, SoldItem, Stock
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = '__all__'
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = '__all__'
+
+
+class ShippingAddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShippingAddress
+        fields = '__all__'
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+
+class WishListItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WishListItem
+        fields = '__all__'
+
+
+class PurchasedItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PurchasedItem
+        fields = '__all__' 
+
+
+class SoldItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SoldItem
+        fields = '__all__' 
+
+
+class StockSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Stock
+        fields = '__all__'
