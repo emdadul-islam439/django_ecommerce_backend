1462921f464cb31f6cd7f2a5bc32a99ebc185c88
Emdadul_Islam <emdadulislam439@gmail.com>
2023-01-12 23:08:58 +0600

    made changes 'Customer'

--- ecommerce_backend/customer_app/models.py
+++ ecommerce_backend/customer_app/models.py
@@ -11,9 +11,10 @@ class AdminUser(models.Model):
     
 class Customer(models.Model):
     id = models.AutoField(primary_key=True)
+    # username, email, password -> in the 'user' table
     user = models.OneToOneField(User, on_delete=models.CASCADE, default='', related_name='customer', null=True)
-    name = models.CharField(max_length=200, null=True)
-    email = models.EmailField(max_length=200, null=True)
+    first_name = models.CharField(max_length=200, null=True)
+    last_name = models.CharField(max_length=200, null=True)
     image = models.ImageField(default='default.png', upload_to="profile_pics")
     
     def __str__(self) -> str:
