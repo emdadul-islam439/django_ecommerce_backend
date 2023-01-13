25af5a3da8a44006375af297371a39cdaeded22f
Emdadul_Islam <emdadulislam439@gmail.com>
2023-01-13 20:58:32 +0600

    successfully added put functionality in 'profile' api

--- ecommerce_backend/customer_app/api/views.py
+++ ecommerce_backend/customer_app/api/views.py
@@ -63,6 +63,15 @@ class ProfileAV(APIView):
         except:
             return Response(status.HTTP_400_BAD_REQUEST)
     
+    def put(self, request):
+        customer_info = Customer.objects.filter(user=request.user).first()
+        serializer = CustomerSerializer(customer_info, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
     
 class WishListAV(generics.ListAPIView):
     serializer_class = WishListItemSerializer
